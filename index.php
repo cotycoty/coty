6a7f09f73f8135f0485e3db9f77fb4bf6391d7aa
coty <69890577+cotycoty@users.noreply.github.com>
2021-06-05 16:04:22 +0800

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/t.html');
+	header('Location: '.$uri.'/index.html');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
